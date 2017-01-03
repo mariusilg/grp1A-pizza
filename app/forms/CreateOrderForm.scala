5ef9de17955df5bc1867bc20774150040b2e30db
Yannic Fraebel <fraebel@hm.edu>
2017-01-03 22:33:21 +0100

    Some minor changes

--- app/forms/CreateOrderForm.scala
+++ app/forms/CreateOrderForm.scala
@@ -5,7 +5,7 @@ package forms
   * @param itemID id of the item.
   * @param quantity quantity of item.
   * @param size size of item.
-  * @param eQuantity optional quantity of extra.
+  * // @param eQuantity optional quantity of extra.
   * @param extraID optional id of extra.
   */
 //case class CreateOrderForm(itemID: Long, quantity: Int, size: Int, eQuantity: Option[Int], extraID: Option[Long])
--- app/models/Order.scala
+++ app/models/Order.scala
@@ -4,7 +4,7 @@ import java.util.Date
   * Order entity.
   * @param id database id of the order.
   * @param custID database id of the customer.
-  * @param orderItem item of order
+  * @param orderItems item of order
   * @param costs costs of order.
   */
 case class Order(var id: Long, var custID: Long, var date: Date, var orderItems: List[OrderItem], var costs: Int) {
